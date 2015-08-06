f1e67f8c917441a81ab32201323a70a92d98c400
elisabettan <ElisabetJohansson@Elisabets-MacBook-Pro.local>
2015-08-06 16:26:17 +0200

    admin kod tillagd i projekt

--- quizsite/urls.py
+++ quizsite/urls.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.conf.urls import include
 
 from django.conf.urls import url
 from quiz import views
@@ -7,5 +8,5 @@ urlpatterns = [
 	url(r"^quiz/([a-z-]+)/$", views.quiz, name="quiz_page"),
 	url(r"^quiz/([a-z-]+)/question/([0-9]+)/$", views.question, name="question_page"),
 	url(r"^quiz/([a-z-]+)/completed/$", views.completed, name="completed_page"),
-	url(r'admin/', include(admin.site.urls))
+	url(r'admin/', include(admin.site.urls)),
 ]
