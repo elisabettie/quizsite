5ff5f84315ca890bb81b151c1c7af32f3bc10d02
elisabettan <ElisabetJohansson@Elisabets-MacBook-Pro.local>
2015-08-06 12:57:42 +0200

    bug def quiz fixad

--- quiz/views.py
+++ quiz/views.py
@@ -23,7 +23,11 @@ def startpage(request):
 	return render(request, "quiz/quizkampenstartsida.html", context)
 
 def quiz(request,slug):
-	return render(request, "quiz/quizkampen.html")
+	context = {
+		"quiz": quizzes[slug],
+		"quiz_slug": slug,
+	}
+	return render(request, "quiz/quizkampen.html", context)
 
 def question(request, slug, number):
 	context = {
