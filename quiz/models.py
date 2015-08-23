b1bd7d8ed17f7d1551c4975aaa06062491e0890b
Elisabet <elisabetajohansson@hotmail.se>
2015-08-23 13:23:39 +0200

    able to do quiz firefox

--- quiz/models.py
+++ quiz/models.py
@@ -19,3 +19,11 @@ class Question(models.Model):
 
 	def __unicode__(self):
 		return self.quiz.name + "/" + self.question
+
+#class Question(models.Model):
+#	quiz = models.ForeignKey(Quiz, related_name="questions")
+#	question = models.TextField()
+#	answer1 = models.CharField(max_length=100)
+#	answer2 = models.CharField(max_length=100)
+#	answer3 = models.CharField(max_length=100)
+#	correct = models.PositiveIntegerField()
